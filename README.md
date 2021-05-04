558d030d25401154a9be46ff69765f98a8d8681d
aashish <aashj99@gmail.com>
2021-05-03 22:05:12 -0400

    [feat] add autotrading

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project name
+# Llama
 
 ![GitHub repo size](https://img.shields.io/github/issues/safekhawaja/192autotrader)
 ![GitHub contributors](https://img.shields.io/github/forks/safekhawaja/192autotrader)
@@ -12,39 +12,24 @@ This is built by Saif Khawaja and Aashish Jain for CIS 192.
 ## Prerequisites
 
 1. Install the packages denoted in `requirements.txt`
+2. Run analyzer.py
 
+## Installing Llama
 
-## Installing <project_name>
+Install the requirements via pip3 that are present in requirements.txt
 
-To install <project_name>, follow these steps:
-
-Linux and macOS:
-```
-<install_command>
-```
-
-Windows:
-```
-<install_command>
-```
-## Using <project_name>
-
-To use <project_name>, follow these steps:
+## Using Llama
 
 ```
-<usage_example>
+python3 analyzer.py
 ```
 
-Add run commands and examples you think users will find useful. Provide an options reference for bonus points!
-
-## Contributing to <project_name>
-<!--- If your README is long or you have some specific process or steps you want contributors to follow, consider creating a separate CONTRIBUTING.md file--->
-To contribute to <project_name>, follow these steps:
+## Contributing to Llama
 
 1. Fork this repository.
 2. Create a branch: `git checkout -b <branch_name>`.
 3. Make your changes and commit them: `git commit -m '<commit_message>'`
-4. Push to the original branch: `git push origin <project_name>/<location>`
+4. Push to the original branch: `git push origin Llama/<location>`
 5. Create the pull request.
 
 Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
@@ -53,17 +38,10 @@ Alternatively see the GitHub documentation on [creating a pull request](https://
 
 Thanks to the following people who have contributed to this project:
 
-* [@scottydocs](https://github.com/scottydocs) 📖
-* [@cainwatson](https://github.com/cainwatson) 🐛
-* [@calchuchesta](https://github.com/calchuchesta) 🐛
+* [@safekhawaja](https://github.com/safekhawaja) 📖
+* [@AashJ](https://github.com/AashJ) 🐛
 
-You might want to consider using something like the [All Contributors](https://github.com/all-contributors/all-contributors) specification and its [emoji key](https://allcontributors.org/docs/en/emoji-key).
 
 ## Contact
 
-If you want to contact me you can reach me at <your_email@address.com>.
-
-## License
-<!--- If you're not sure which open license to use see https://choosealicense.com/--->
-
-This project uses the following license: [<license_name>](<link>).
+If you want to contact me you can reach me at aashj99@seas.upenn.edu, or saifnk@wharton.upenn.edu.
--- analyzer.py
+++ analyzer.py
@@ -1,13 +1,26 @@
+import json
+import os
 from math import sqrt
 
 import pandas as pd
+import requests
 from statsmodels.tsa.arima.model import ARIMA
 from pandas import DataFrame
 from sklearn.metrics import mean_squared_error
+import alpaca_trade_api as tradeapi
+
+
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID': os.getenv('APCA_API_KEY_ID'), 'APCA-API-SECRET-KEY': os.getenv('APCA_API_SECRET_KEY')}
+
 
 class Analyzer:
     def __init__(self, pathToData: str) -> None:
         super().__init__()
+        self.api = tradeapi.REST()
+        self.predictions = list()
         self.df = pd.read_csv(pathToData, index_col=0, parse_dates=True)
         self.model = ARIMA(self.df.High, order=(5, 1, 0))
         self.model_fit = self.model.fit()
@@ -27,18 +40,55 @@ class Analyzer:
 
     def train_and_predict(self, train_data, test_data):
         history = [x for x in train_data]
-        predictions = list()
+        i = 0;
         for t in range(len(test_data)):
             model = ARIMA(history, order=(5, 1, 0))
             model_fit = model.fit()
             output = model_fit.forecast()
             y_hat = output[0]
-            predictions.append(y_hat)
+
+            if i % 1000 == 0 and i != 0:
+                [open, close] = self.get_current_day_price('GOOGL')
+                if y_hat > open:
+                    print('Execute order! 🏦🏦🏦🏦')
+                    self.create_order('GOOGL', 1, 'buy', 'market', 'gtc')
+                else:
+                    print('Buy if you wanna lose money')
+
+            self.predictions.append(y_hat)
             obs = test_data[t]
             history.append(obs)
             print('predicted=%f, expected=%f' % (y_hat, obs))
-        rmse = sqrt(mean_squared_error(test_data, predictions))
+            i += 1
+
+        rmse = sqrt(mean_squared_error(test_data, self.predictions))
         print('Test RMSE: %.3f' % rmse)
+
+
+    def create_order(self, symbol, qty, side, type, time_in_force):
+        data = {
+            "symbol": symbol,
+            "qty": qty,
+            "side": side,
+            "type": type,
+            "time_in_force": time_in_force
+        }
+
+        r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+
+        return json.loads(r.content)
+
+    def get_orders(self):
+        r = requests.get(ORDERS_URL, headers=HEADERS)
+        return json.loads(r.content)
+
+    def get_current_day_price(self, symbol):
+        barset = self.api.get_barset(symbol, 'day', limit=1)
+        bars = barset[symbol]
+
+        open = bars[0].o
+        close = bars[-1].c
+        return [open, close]
 
 
 if __name__ == '__main__':
